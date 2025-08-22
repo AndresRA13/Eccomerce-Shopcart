1d769059af288695ebd4bcdafa6c3e70aa30c25b
Andres Rojas <rojasaguileraandres13@gmail.com>
2025-08-22 09:42:48 -0500

    cambios en hast router

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import { CartProvider } from "./context/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Eccomerce-Shopcart/">
+    <HashRouter>
     <CartProvider>
     <App />
     </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
